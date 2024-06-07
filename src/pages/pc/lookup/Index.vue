732c8bed74089a379afeb04940802288fb511ea4
huayi <1342881632@qq.com>
2024-06-07 13:36:30 +0800

    hhh

--- src/pages/pc/lookup/Index.vue
+++ src/pages/pc/lookup/Index.vue
@@ -1,21 +1,13 @@
 <template>
-  <img :src="indexImg" @click="linkImg">
+  <button @click="redirectToAnotherProject">确定跳转</button>
 </template>
 
 <script>
 export default {
-  name: "index",
-
-  data() {
-    return {
-      indexImg:"https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2755875330,2937693649&fm=26&gp=0.jpg",
-      indexro:"www.baidu.com"
-    }
-  },
   methods: {
-    linkImg(){
-      window.open(this.indexro,'_blank') // 在新窗口打开外链接
-      window.location.href =this.indexro;  //在本页面打开外部链接
+    redirectToAnotherProject() {
+      const returnUrl = window.location.href;
+      window.location.href = 'http://www.baidu.com'
     }
   }
 }
