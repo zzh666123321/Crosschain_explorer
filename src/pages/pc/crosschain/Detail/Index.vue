945e1564882d7913647b6c7275121a3cfce006ca
huayi <1342881632@qq.com>
2024-06-07 16:41:08 +0800

    跨链部分mock完成了，就差后端了

--- src/pages/pc/crosschain/Detail/Index.vue
+++ src/pages/pc/crosschain/Detail/Index.vue
@@ -2,36 +2,24 @@
   <div>
     <Title title="交易详情"></Title>
     <div class="info">
-      <Item title="交易哈希">{{ hash }}</Item>
-      <Item title="状态">
-        <a-tag v-if="transaction?.status === '1'" color="#000"> 已确认 </a-tag>
-        <a-tag v-else color="default"> 未确认 </a-tag>
+      <Item title="源链IP">{{ transaction?.srcIp}}</Item>
+
+
+      <Item title="源链端口">{{ transaction?.srcPort }}</Item>
+      <Item title="源链Hash">
+        <div>{{ transaction?.srcHash }}</div>
       </Item>
-      <Item title="区块高度">{{ transaction?.height }}</Item>
-      <Item title="Nonce">
-        <div>{{ transaction?.nonce }}</div>
+      <Item title="目的链IP">
+        <div>{{ transaction?.dstIp }}</div>
       </Item>
-      <Item title="发送者">
-        <router-link :to="'/address/' + transaction?.from">{{
-          transaction?.from
-        }}</router-link>
+      <Item title="目的链端口">
+        <div>{{ transaction?.dstPort }}</div>
       </Item>
-      <Item title="接收方">
-        <router-link :to="'/address/' + transaction?.to">{{
-          transaction?.to
-        }}</router-link>
+      <Item title="目的链Hash">
+        <div>{{ transaction?.dstHash }}</div>
+
       </Item>
-      <Item title="签名">
-        <!-- <div>r : <a-tag> {{transaction?.r}} </a-tag></div>
-            <div style="margin: 6px 0;">s : <a-tag> {{transaction?.s}} </a-tag></div>
-            <div>v : <a-tag> {{transaction?.v}} </a-tag></div> -->
-        <a-tag> {{ transaction?.signature }} </a-tag>
-      </Item>
-      <Item title="存证">
-        <div class="text-area">
-          {{ transaction?.data }}
-        </div>
-      </Item>
+
     </div>
   </div>
 
@@ -42,28 +30,30 @@ import { detail } from "@/api/transaction";
 import { ref, reactive } from "vue";
 import Title from "@/components/Title.vue";
 import Item from "@/components/Item.vue";
-
+import tx from "@/mock/singleTx.json";
 const hasResult = ref(false);
+
 
 const props = defineProps({
   hash: String,
 });
 
-const transaction = ref<API.TransactionDetail>({});
+const transaction = tx.data
+// const transaction = ref<API.TransactionDetail>({});
 
 const params = reactive({
   tx_hash: props.hash || "123",
 });
 
-detail(params).then((res) => {
-  if (res && res.data.tx) {
-    transaction.value = res.data.tx;
-    hasResult.value = true;
-    
-  } else {
-    console.log("查询结果为空");
-  }
-});
+// detail(params).then((res) => {
+//   if (res && res.data.tx) {
+//     transaction.value = res.data.tx;
+//     hasResult.value = true;
+//
+//   } else {
+//     console.log("查询结果为空");
+//   }
+// });
 </script>
 
 <style scoped>
